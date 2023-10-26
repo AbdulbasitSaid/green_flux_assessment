08c377ba09ee1c9799ac99a6242bec31933611e1
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2023-10-26 11:55:47 +0200

    added test

--- test/features/charge_locations/domain/locations_repository_test.dart
+++ test/features/charge_locations/domain/locations_repository_test.dart
@@ -9,31 +9,23 @@ class MockLocationsApi extends Mock implements LocationsApi {}
 class MockLocationsRepository extends Mock implements LocationsRepository {}
 
 void main() {
-  late LocationsRepository
-      locationsRepository; // Changed to LocationsRepository from MockLocationsRepository
+  late LocationsRepository locationsRepository;
   late MockLocationsApi mockLocationsApi;
   late List<Location> mockLocations;
   const query = 'query';
 
   setUp(() {
     mockLocationsApi = MockLocationsApi();
-    locationsRepository = LocationsRepository(
-        locationsApi: mockLocationsApi); // Pass the mockLocationsApi
-    mockLocations = <Location>[/* some mock locations */];
+    locationsRepository = LocationsRepository(locationsApi: mockLocationsApi);
+    mockLocations = <Location>[];
 
-    // Necessary to define a stub for the mock method that returns a generic type.
     when(() => mockLocationsApi.searchChargeLocation(query))
         .thenAnswer((invocation) async => mockLocations);
   });
 
   test('getChargeLocations returns a list of locations', () async {
-    // Arrange
-    // You might want to have some arrangement here if needed.
-
-    // Act
     final result = await locationsRepository.getChargeLocations(query: query);
 
-    // Assert
     expect(result, equals(mockLocations));
     verify(() => mockLocationsApi.searchChargeLocation(query)).called(1);
   });
